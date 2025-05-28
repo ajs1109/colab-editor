b2680aaf997acaf7a64c2eac8d54c81826f96af0
ajs1109 <78317286+ajs1109@users.noreply.github.com>
2025-05-28 22:34:09 +0530

    Added src routing

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     domains: [
-      'bfcvoikogyjfeqxzuaob.supabase.co', // Your Supabase storage domain
+      'flybjlchqkqochkvhyca.supabase.co', // Your Supabase storage domain
       'avatars.githubusercontent.com', // For GitHub avatars
       'lh3.googleusercontent.com', // For Google avatars
     ],
--- src/components/TextEditor/Toolbar/Mention/suggestion.ts
+++ src/components/TextEditor/Toolbar/Mention/suggestion.ts
@@ -1,5 +1,5 @@
 import { ReactRenderer } from '@tiptap/react';
-//import tippy, { Instance, Props } from 'tippy.js';
+import tippy, { Instance, Props } from 'tippy.js';
 import MentionList, { MentionListRef } from './MentionList';
 
 interface SuggestionProps {
@@ -25,7 +25,7 @@ const getSuggestion = (users: Partial<IUser>[]) => ({
 
   render: () => {
     let component: ReactRenderer<MentionListRef> | null = null;
-    //let popup: Instance<Props> | null = null;
+    let popup: Instance<Props> | null = null;
 
     return {
       onStart: (props: SuggestionProps) => {
@@ -38,22 +38,22 @@ const getSuggestion = (users: Partial<IUser>[]) => ({
           return;
         }
 
-        // popup = tippy('body', {
-        //   getReferenceClientRect: props.clientRect,
-        //   appendTo: () => document.body,
-        //   content: component.element,
-        //   showOnCreate: true,
-        //   interactive: true,
-        //   trigger: 'manual',
-        //   placement: 'bottom-start',
-        //   onShow: () => {
-        //     // Focus the first button when the tooltip is shown
-        //     const firstButton = document.querySelector(
-        //       '.mention-btn-0'
-        //     ) as HTMLButtonElement;
-        //     firstButton?.focus();
-        //   },
-        // })[0];
+        popup = tippy('body', {
+          getReferenceClientRect: props.clientRect,
+          appendTo: () => document.body,
+          content: component.element,
+          showOnCreate: true,
+          interactive: true,
+          trigger: 'manual',
+          placement: 'bottom-start',
+          onShow: () => {
+            // Focus the first button when the tooltip is shown
+            const firstButton = document.querySelector(
+              '.mention-btn-0'
+            ) as HTMLButtonElement;
+            firstButton?.focus();
+          },
+        })[0];
       },
 
       onUpdate(props: SuggestionProps) {
@@ -63,9 +63,9 @@ const getSuggestion = (users: Partial<IUser>[]) => ({
           return;
         }
 
-        // popup?.setProps({
-        //   getReferenceClientRect: props.clientRect || props.clientRect,
-        // });
+        popup?.setProps({
+          getReferenceClientRect: props.clientRect || props.clientRect,
+        });
       },
 
       onKeyDown(props: OnKeyDownProps) {
@@ -78,7 +78,7 @@ const getSuggestion = (users: Partial<IUser>[]) => ({
       },
 
       onExit() {
-        // popup?.destroy();
+        popup?.destroy();
         component?.destroy();
       },
     };
